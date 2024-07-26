0effaf072fd123a21a2693bb664f75941f7ff2da
William <victorwccv@gmail.com>
2024-07-26 18:52:31 -0500

    feat:

--- client/src/components/Button.jsx
+++ client/src/components/Button.jsx
@@ -1,8 +1,19 @@
-const Button = ({ label, addStyles = "",type = "button", icon }) => {
+const Button = ({
+  type = "button",
+  label,
+  icon,
+  title,
+  onClick,
+  width = "fit-content",
+  color = "bg-[var(--color-accent)]",
+  textColor = "text-[var(--color-background)]",
+}) => {
   return (
     <button
       type={type}
-      className={`bg-blue-500 text-white font-semibold py-2 px-4 rounded-full ${addStyles}`}
+      className={`font-semibold py-2 px-4 rounded-full hover:scale-105 shadow-md ${color} ${textColor} ${width}`}
+      onClick={onClick}
+      title={title}
     >
       {icon}
       {label}
