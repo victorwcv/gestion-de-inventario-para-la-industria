bcfb159af446452fd96601b6ebacffa61b5f23fc
William <victorwccv@gmail.com>
2024-07-26 19:02:37 -0500

    Add icons.jsx file to import all the icons that I will use in the project

---
+++ client/src/icons/icons.jsx
@@ -0,0 +1,31 @@
+import {
+  FaArrowLeft,
+  FaArrowRight,
+  FaBars,
+  FaClipboardCheck,
+  FaHardHat,
+  FaHome,
+  FaRegEdit,
+  FaRegTrashAlt,
+  FaSignOutAlt,
+  FaToolbox,
+  FaTools,
+  FaUser,
+} from "react-icons/fa";
+
+const icons = {
+  menu: <FaBars />,
+  home: <FaHome />,
+  tool: <FaTools />,
+  toolBox: <FaToolbox />,
+  hat: <FaHardHat />,
+  clipboard: <FaClipboardCheck />,
+  arrowLeft: <FaArrowLeft />,
+  arrowRight: <FaArrowRight />,
+  signOut: <FaSignOutAlt />,
+  user: <FaUser />,
+  regEdit: <FaRegEdit />,
+  faRegTrash: <FaRegTrashAlt />,
+};
+
+export default icons;
