32fcfdc8a7f8465ff2663a732a432477d8aa6138
William <victorwccv@gmail.com>
2024-07-27 21:27:50 -0500

    add a new prop to disable the field

--- client/src/components/FormField.jsx
+++ client/src/components/FormField.jsx
@@ -1,6 +1,6 @@
 import { Field, ErrorMessage } from "formik";
 
-const FormField = ({ formik, label, name, type }) => {
+const FormField = ({ formik, label, name, type, disabled }) => {
   return (
     <div className="relative pb-6 flex flex-col">
       <label htmlFor={name} className="pl-2 mb-1">{label}</label>
@@ -8,7 +8,8 @@ const FormField = ({ formik, label, name, type }) => {
         id={name}
         name={name}
         type={type}
-        className={`p-2 bg-white bg-opacity-45 focus:border-blue-500
+        disabled={disabled}
+        className={`p-2 bg-white bg-opacity-45 focus:border-[var(--color-accent)]
           ${
           formik.errors[name] && formik.touched[name] ? "  border-red-500" : ""
         } 
